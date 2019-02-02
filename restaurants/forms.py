39004808ae2952c065891a4cd2c7d2eaf75d7292
NatalieBlack <natalievictoriablack@gmail.com>
2019-02-02 18:26:05 -0500

    Separate date and time fields for reservation form

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm, DateTimeField
+from datetime import date
+from django.forms import ModelForm, TimeInput, DateInput
 from restaurants.models import Reservation
 
 class ReservationForm(ModelForm):
 
     class Meta:
         model = Reservation
-        fields = ['party_size', 'datetime', 'notes']
+        fields = ['party_size', 'date', 'time', 'notes']
+        widgets = {
+            'time': TimeInput(attrs={'type': 'time' }),
+            'date': DateInput(attrs={'type': 'date', 'min': date.today() })
+        }
