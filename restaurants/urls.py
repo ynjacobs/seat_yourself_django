a8e8802988280101a9e0f3c1b77feecd0f13ae6c
NatalieBlack <natalievictoriablack@gmail.com>
2019-02-02 01:09:05 -0500

    Restaurant show page

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,4 +3,5 @@ import restaurants.views as views
 
 urlpatterns = [
     path('', views.restaurants_list, name='restaurants_list'),
+    path('<int:id>', views.restaurant_show, name='restaurant_show'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -5,3 +5,8 @@ def restaurants_list(request):
     restaurants = Restaurant.objects.all()
     context = {'restaurants': restaurants, 'title': 'Restaurants'}
     return render(request, 'index.html', context)
+
+def restaurant_show(request, id):
+    restaurant = Restaurant.objects.get(pk=id)
+    context = {'restaurant': restaurant, 'title': restaurant.name}
+    return render(request, 'restaurant_details.html', context)
