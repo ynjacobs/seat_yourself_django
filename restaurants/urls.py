4b52e0909f5c2cdce1ab1c27f4fa32342a38cc8f
NatalieBlack <natalievictoriablack@gmail.com>
2019-02-02 21:28:11 -0500

    Add leading forward slash to restaurant URL patterns

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,6 +3,6 @@ import restaurants.views as views
 
 urlpatterns = [
     path('', views.restaurants_list, name='restaurants_list'),
-    path('<int:id>', views.restaurant_show, name='restaurant_show'),
-    path('<int:restaurant_id>/reservations/create', views.reservation_create, name='reservation_create'),
+    path('<int:id>/', views.restaurant_show, name='restaurant_show'),
+    path('<int:restaurant_id>/reservations/create/', views.reservation_create, name='reservation_create'),
 ]
